b6f48639f5092d4d533e2fc9cdef243a0202db5b
Luis Felipe Palacio Salamanca <luis.palacio@evertecinc.com>
2025-05-16 10:10:22 -0500

    feat: add logic to update budget when create a transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\CategoryTypes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -25,5 +26,24 @@ class Transaction extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::created(function ($transaction) {
+            if ($transaction->type === CategoryTypes::EXPENSE->value) {
+                $budget = Budget::query()
+                    ->where('user_id', $transaction->user_id)
+                    ->where('category_id', $transaction->category_id)
+                    ->where('month', now()->format('F'))
+                    ->where('year', now()->year)
+                    ->first();
+
+                if ($budget) {
+                    $budget->spent_amount += $transaction->amount;
+                    $budget->save();
+                }
+            }
+        });
     }
 }
